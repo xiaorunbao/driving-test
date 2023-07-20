7cccdbbf234f702c07aab7a87ab2058f0aef43a3
rainbow <542929781@qq.com>
2023-07-20 16:17:21 +0800

    style(custom): format json

--- .cz-config.js
+++ .cz-config.js
@@ -1,53 +1,17 @@
 module.exports = {
   types: [
-    {
-      value: 'feat',
-      name: 'feat: 新功能',
-    },
-    {
-      value: 'fix',
-      name: 'fix: 修复bug',
-    },
-    {
-      value: 'init',
-      name: 'init: 初始化',
-    },
-    {
-      value: 'docs',
-      name: 'docs: 文档变更',
-    },
-    {
-      value: 'style',
-      name: 'style: 代码的样式美化',
-    },
-    {
-      value: 'refactor',
-      name: 'refactor: 重构',
-    },
-    {
-      value: 'perf',
-      name: 'perf: 性能优化',
-    },
-    {
-      value: 'test',
-      name: 'test: 测试',
-    },
-    {
-      value: 'revert',
-      name: 'revert: 回退',
-    },
-    {
-      value: 'build',
-      name: 'build: 打包',
-    },
-    {
-      value: 'chore',
-      name: 'chore: 构建/工程依赖/工具',
-    },
-    {
-      value: 'ci',
-      name: 'ci: CI related changes',
-    },
+    { value: 'feat', name: 'feat: 新功能' },
+    { value: 'fix', name: 'fix: 修复bug' },
+    { value: 'init', name: 'init: 初始化' },
+    { value: 'docs', name: 'docs: 文档变更' },
+    { value: 'style', name: 'style: 代码的样式美化' },
+    { value: 'refactor', name: 'refactor: 重构' },
+    { value: 'perf', name: 'perf: 性能优化' },
+    { value: 'test', name: 'test: 测试' },
+    { value: 'revert', name: 'revert: 回退' },
+    { value: 'build', name: 'build: 打包' },
+    { value: 'chore', name: 'chore: 构建/工程依赖/工具' },
+    { value: 'ci', name: 'ci: CI related changes' },
   ],
   messages: {
     type: '请选择提交类型(必填)',
@@ -60,5 +24,5 @@ module.exports = {
   },
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'], // 当提交类型为feat、fix时才有破坏性修改选项
-  subjectLimit: 72,
+  subjectLimit: 100,
 };
